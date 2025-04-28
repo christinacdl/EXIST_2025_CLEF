551bb075eab94ac68375ce945bd59c68f9a5bd45
Christina Christodoulou <80356232+christinacdl@users.noreply.github.com>
2025-04-29 01:24:45 +0300

    Update run_process_data.sh

--- run_process_data.sh
+++ run_process_data.sh
@@ -2,7 +2,8 @@
 
 train_dir='EXIST_2025_Tweets_Dataset/training'
 dev_dir='EXIST_2025_Tweets_Dataset/dev'
-output_dir='EXIST_2025_Tweets_Dataset/processed'
+test_dir='EXIST_2025_Tweets_Dataset/test'
+output_dir='EXIST_2025_Tweets_Dataset/mixed'
 language='all'
 text_column='tweet'
 label_column='hard_label' 
@@ -11,6 +12,7 @@ evaluation_type='hard'
 python process_data.py \
     --train_dir ${train_dir} \
     --dev_dir ${dev_dir} \
+    --test_dir ${test_dir} \
     --output_dir ${output_dir} \
     --language ${language} \
     --text_column ${text_column} \
