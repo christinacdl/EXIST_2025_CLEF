a940f0a8fcb963554ce18807341547700891c369
Christina Christodoulou <80356232+christinacdl@users.noreply.github.com>
2025-05-02 11:36:45 +0300

    Update run_LLM.sh

--- run_LLM.sh
+++ run_LLM.sh
@@ -3,8 +3,10 @@ export CUDA_VISIBLE_DEVICES=1,0
 CUDA_LAUNCH_BLOCKING=1,0
 
 evaluation_type='hard'
-llm_predictions='llm_predictions_llama32_3B.json'
+llm_predictions='llm_TEST_predictions_llama3_2_definitions_3b.json'
+input_texts_file='EXIST_2025_Tweets_Dataset/mixed/test_no_labels_preprocessed.csv'
 
 python LLM_prompting.py \
     --evaluation_type ${evaluation_type} \
-    --llm_predictions ${llm_predictions}
+    --llm_predictions ${llm_predictions} \
+    --input_texts_file ${input_texts_file}
